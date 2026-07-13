e901ce10ebecf2efed9a8e5fddb510aa717fe0d4
enikov1 <support-s-game@yandex.ru>
2026-07-13 21:39:13 +0500

    Upd 1.3

--- app/Http/Controllers/AdminPanelController.php
+++ app/Http/Controllers/AdminPanelController.php
@@ -14,6 +14,13 @@ class AdminPanelController extends Controller
             abort(404);
         }
 
+        if ($spaPath === null && !str_ends_with($request->getPathInfo(), '/')) {
+            $query = $request->getQueryString();
+            $target = AdminPath::base() . '/' . ($query ? '?' . $query : '');
+
+            return redirect($target, 301);
+        }
+
         $indexPath = public_path('admin/index.html');
         if (!is_file($indexPath)) {
             return response(
@@ -23,8 +30,11 @@ class AdminPanelController extends Controller
             );
         }
 
+        $adminBase = AdminPath::base();
+        $baseHref = htmlspecialchars($adminBase . '/', ENT_QUOTES, 'UTF-8');
         $html = file_get_contents($indexPath);
-        $inject = '<script>window.__ADMIN_BASE__=' . json_encode(AdminPath::base()) . ';</script>';
+        $inject = '<base href="' . $baseHref . '">'
+            . '<script>window.__ADMIN_BASE__=' . json_encode($adminBase) . ';</script>';
         $html = str_replace('</head>', $inject . '</head>', $html);
 
         return response($html, 200, ['Content-Type' => 'text/html; charset=UTF-8']);
